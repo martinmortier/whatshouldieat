c67581abc436d71ece827335658bd919230ff1af
Mortier Martin <34869851+martinmortier@users.noreply.github.com>
2021-08-29 22:04:56 +0200

    Remove react-router-native

--- App.tsx
+++ App.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './redux/store'
+import { NativeRouter, Route, Link } from 'react-router-native';
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <NativeRouter>
+        <View style={styles.container}>
+          <Text>Open up App.tsx to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </NativeRouter>
     </Provider>
   );
 }
