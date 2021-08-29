f834f451d215ae88d0fc10c23a5ba0fd2ff634c3
Mortier Martin <34869851+martinmortier@users.noreply.github.com>
2021-08-29 22:05:47 +0200

    Refactor

--- App.tsx
+++ App.tsx
@@ -3,16 +3,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './redux/store'
-import { NativeRouter, Route, Link } from 'react-router-native';
 export default function App() {
   return (
     <Provider store={store}>
-      <NativeRouter>
-        <View style={styles.container}>
-          <Text>Open up App.tsx to start working on your app!</Text>
-          <StatusBar style="auto" />
-        </View>
-      </NativeRouter>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
     </Provider>
   );
 }
