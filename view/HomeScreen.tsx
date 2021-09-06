5e2c97202e73b20a82d458e71183a7b60bad744f
Mortier Martin <34869851+martinmortier@users.noreply.github.com>
2021-09-06 23:52:03 +0200

    Update HomeScreen.tsx

--- view/HomeScreen.tsx
+++ view/HomeScreen.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import { Button, Text, View } from "react-native"
-const HomeScreen = ({ navigation }) => {
+const HomeScreen = () => {
     return(
         <View style={{padding:'10%'}}>
             <Text>Home Screen</Text>
-            <Button title="Go to Details" onPress={() => {
-                navigation.navigate('Dish')
-            }}/>
         </View>
     )
 }
