80e09b1c03ec0b86dac65840860f76142365f6ae
Mortier Martin <34869851+martinmortier@users.noreply.github.com>
2021-09-12 21:32:02 +0200

    Fix algo for random dishes of the week

--- components/DishesByWeek.tsx
+++ components/DishesByWeek.tsx
@@ -2,7 +2,7 @@ import { Route } from "@react-navigation/routers";
 import React from "react";
 import { ScrollView, View } from "react-native";
 import { IDish } from "../interfaces/IDish";
-import { Text } from 'react-native-elements'
+import { Text } from "react-native-elements";
 import { getRandomDishes } from "../services/dishService";
 import DishDetails from "./DishDetails";
 import RandomDishes from "./RandomDishes";
@@ -10,7 +10,7 @@ import RandomDishes from "./RandomDishes";
 type DishesByWeekProps = {
   route: { params: { dishes: IDish[] } };
 };
-const DishesByWeek = ({ route } : DishesByWeekProps) => {
+const DishesByWeek = ({ route }: DishesByWeekProps) => {
   const { dishes } = route.params;
   const dayOfWeek: string[] = [
     "Monday",
@@ -22,16 +22,17 @@ const DishesByWeek = ({ route } : DishesByWeekProps) => {
     "Sunday",
   ];
   return (
-    //TODO: Algo not working
     <ScrollView>
-      {/* {getRandomDishes(dishes, 7).map((dish: IDish) => (
-        dayOfWeek.map((day: string, index: number) => (
+      {dayOfWeek.map((day: string, index: number) =>
+        getRandomDishes(dishes, 1).map((dish) => (
           <>
-            <Text h3 key={index}>{day}</Text>
+            <Text h3 key={index}>
+              {day}
+            </Text>
             <DishDetails dish={dish} />
           </>
         ))
-      ))} */}
+      )}
     </ScrollView>
   );
 };
