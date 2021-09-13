559e7d3f137357746d70e217f5c08f19ba3fa77d
Mortier Martin <34869851+martinmortier@users.noreply.github.com>
2021-09-13 23:53:24 +0200

    styling input

--- view/FindDishScreen.tsx
+++ view/FindDishScreen.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { View } from "react-native";
-import { Text, Input, Icon } from "react-native-elements";
+import { Text, Input, Icon, Button } from "react-native-elements";
 import DisplayAllDishes from "../components/DisplayAllDishes";
 import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks";
 import { filterDishes, initialize } from "../redux/allDishesReducer";
@@ -23,14 +23,16 @@ const FindDishScreen = ({ navigation }: FindDishScreenProps) => {
   return (
     <View>
       {dishes ? (
-        <View>
+        <View style={styles.main}>
           <Text h1>Find foods !</Text>
+          <View style={styles.input}>
           <Input
             placeholder="Search dishes"
             onChangeText={(text) => handleChangeText(text)}
             value={search}
             leftIcon={<Icon name="search" size={24} color="black" />}
           />
+          </View>
           <DisplayAllDishes dishes={dishes} navigation={navigation} />
         </View>
       ) : (
@@ -39,5 +41,15 @@ const FindDishScreen = ({ navigation }: FindDishScreenProps) => {
     </View>
   );
 };
+
+const styles = {
+  main: {
+    paddingHorizontal:13
+  },
+  input: {
+    backgroundColor:'#f8e8d9',
+    borderRadius: 20,
+  }
+}
 
 export default FindDishScreen;
