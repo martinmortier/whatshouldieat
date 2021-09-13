fab149a03b3db9d117dfb8dcac8c93ec03b60a04
Mortier Martin <34869851+martinmortier@users.noreply.github.com>
2021-09-13 23:53:55 +0200

    styling DisplayAllDishes component

--- components/DisplayAllDishes.tsx
+++ components/DisplayAllDishes.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { View } from "react-native";
-import { Text, Card } from "react-native-elements";
+import { Text, Card, Button, colors } from "react-native-elements";
 import { IDish } from "../interfaces/IDish";
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs'
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 type DisplayAllDishesProps = {
   dishes: IDish[];
   navigation: BottomTabNavigationProp<any>; 
 }
 const DisplayAllDishes = ({dishes, navigation}:DisplayAllDishesProps) => {
   return (
-    <View style={{display:"flex", flexDirection:"row"}}>
+    <View style={styles.main}>
       {dishes?.map(
         (
           dish,
           index // () Return - {} don't
         ) => (
-          <Card key={index}>
+          <Card key={index} containerStyle={{flexBasis:'48%'}}>
             <Card.Title onPress={() => navigation.navigate('DishScreen', { id : dish.id}) }>{dish.name}</Card.Title>
             {dish.ingredients.map((ingredient, index) => (
               <Text key={index}>{ingredient.name}</Text>
@@ -24,6 +24,8 @@ const DisplayAllDishes = ({dishes, navigation}:DisplayAllDishesProps) => {
             <Card.Divider />
             <Text>{dish.time}</Text>
             <Text>{dish.votes}</Text>
+            {/* //TODO: Modify the style */}
+            <Button buttonStyle={{}}>Infos</Button>
           </Card>
         )
       )}
@@ -31,4 +33,15 @@ const DisplayAllDishes = ({dishes, navigation}:DisplayAllDishesProps) => {
   );
 };
 
+const styles = {
+  main: {
+    flex:1,
+    flexDirection:'row',
+    flexWrap:'wrap',
+    textAlign:'center'
+  },
+  items: {
+    flexBasics:'48%'
+  }
+}
 export default DisplayAllDishes;
