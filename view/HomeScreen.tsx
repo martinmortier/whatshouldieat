a004a9983e79b555d26625cce8603578b11a9138
Mortier Martin <34869851+martinmortier@users.noreply.github.com>
2021-09-16 17:30:05 +0200

    styling

--- view/HomeScreen.tsx
+++ view/HomeScreen.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
-import { Button, Text, View } from "react-native"
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { Avatar } from "react-native-elements";
 const HomeScreen = () => {
-    return(
-        <View style={{padding:'10%'}}>
-            <Text>Home Screen</Text>
-        </View>
-    )
-}
+    const avatarURL = 'https://icon-library.com/images/avatar-icon/avatar-icon-14.jpg'
+  return (
+    <View style={styles.main}>
+      <View style={styles.header}>
+        <Text style={{ flexGrow: 1 }}>Hello</Text>
+        <Avatar containerStyle={{marginRight:'10%'}} rounded size="large" source={{uri: avatarURL}} />
+      </View>
+    </View>
+  );
+};
 
-export default HomeScreen
+const styles = StyleSheet.create({
+  main: {
+    display: "flex",
+    paddingTop: "10%",
+    flexWrap: "wrap",
+    flexDirection: "column",
+    backgroundColor:"yellow"
+  },
+  header: {
+    display: "flex",
+    flexWrap: "wrap",
+    flexDirection: "row",
+    // backgroundColor:'blue'
+  },
+  headerText: {
+      //TODO: Terminate the styling
+  }
+});
+
+export default HomeScreen;
