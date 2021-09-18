e974596a28a1ace4faad7601da977917b649785f
Mortier Martin <34869851+martinmortier@users.noreply.github.com>
2021-09-18 12:32:21 +0200

    styling RandomDishesScreen

--- view/RandomDishesScreen.tsx
+++ view/RandomDishesScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { View } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { Button } from "react-native-elements";
 import RandomDishes from "../components/RandomDishes";
 import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks";
@@ -15,21 +15,45 @@ const RandomDishesScreen = ({ navigation }: any) => {
   }, []);
 
   return (
-    <View style={{ paddingTop: "10%" }}>
+    <View style={styles.layout}>
       {dishes.length > 0 && (
         <View>
+          <Text>Make a choice ! </Text>
           <Button
             title="I want a random dish !"
+            buttonStyle={styles.button}
             onPress={() => {
               const dish_id: number = getRandomDishes(dishes, 1)[0].id;
               return navigation.navigate("DishScreen", { id: dish_id });
             }}
           />
-          <Button title="Propose me some dishes for my week" onPress={() => navigation.navigate("DishesByWeek", {dishes: dishes})} />
+          <Button
+            title="Propose me some dishes for my week"
+            buttonStyle={styles.button}
+            onPress={() =>
+              navigation.navigate("DishesByWeek", { dishes: dishes })
+            }
+          />
         </View>
       )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  layout: {
+    display:'flex',
+    flexDirection:'column',
+    justifyContent:'center',
+    alignItems:'center',
+    flexGrow:1
+  },
+  button: {
+    backgroundColor:'#ff8a02',
+    margin:20,
+    borderRadius:10,
+    padding:"5%"
+  }
+});
 
 export default RandomDishesScreen;
